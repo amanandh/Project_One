f6e080404c4ca2e8b3d0b76911bccfe43b253334
amanandhar <ashmamdr11@gmail.com>
2018-03-13 22:32:50 +0000

    Added js finally

--- js/site.js
+++ js/site.js
@@ -4,4 +4,20 @@
 jQuery(function($) {
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
+  $('#age-verification').prop('checked', true);
 });
+
+document.getElementById("submit").addEventListener("click", confirm);
+function confirm(event){
+  if((document.getElementById("fullname").value === "") 
+     || (document.getElementById("email").value === "" ))
+  {
+    alert("Please fill out your name and your email address.");
+    return false;
+  }
+  else
+  {
+    alert("Thank you for entering the giveaway. Good luck!");
+    return true;
+  }
+}
